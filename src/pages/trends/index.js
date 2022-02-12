e54a5f2661ae74da95ad8703c02f5f3e485a4e3c
Alexei Garban <garban.valdeon@gmail.com>
2022-02-12 19:51:57 +0100

    test

--- src/pages/trends/index.js
+++ src/pages/trends/index.js
@@ -4,10 +4,10 @@ import Layout from "../../components/Layout";
 const TrendsPage = ({location}) => {
 
   const [data,setData]=useState([])
-  const OsTrendsReportId=location.pathname
+  const OsTrendsReportId=location?.pathname
   const id=location?.state?.url || ""
 
-  console.log(id)
+
 
   const getTrendsReport = ()=>{
     fetch(`https://websiteserver-ds7cf.ondigitalocean.app/posts/${id}`)
@@ -20,15 +20,14 @@ const TrendsPage = ({location}) => {
       getTrendsReport()
     },[id])
 
-    console.log("data",data)
   
   return (
     <Layout>
       <section className="">
         <div className="container mx-auto py-10">
           <h3 className="text-5xl font-black">{data.title}</h3>
-          <img src={data.featured_image.formats.small.url} alt="" />
-          <div dangerouslySetInnerHTML={{ __html: data.content }}></div>
+          <img src={data?.featured_image?.formats?.small?.url} alt="" />
+          <div dangerouslySetInnerHTML={{ __html: data?.content }}></div>
         </div>
       </section>
     </Layout>
